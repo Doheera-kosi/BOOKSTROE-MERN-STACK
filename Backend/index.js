9ace412c74403319096e584455affd7002c79ce3
Doheera-kosi <kupourevans99@gmail.com>
2024-02-10 14:30:46 +0000

    Modified index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from 'mongoose'
 
 const app = express();
+
+app.get('/', (request, response) => {
+  console.log(request)
+  return response.status(200).send('Welcome to BookStore App')
+});
 
 app.listen(PORT, () => {
   console.log(`App is listening to port: ${PORT}`);
 });
+
+mongoose.connect(mongoDBURL)
