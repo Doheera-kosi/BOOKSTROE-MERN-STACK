5945520f3104d2ae66316557a9f4d400db2ab80b
Doheera-kosi <kupourevans99@gmail.com>
2024-02-10 22:49:00 +0000

    Modify index.js

--- Backend/index.js
+++ Backend/index.js
@@ -55,6 +55,19 @@ app.get('/books', async (request, response) => {
   }
 });
 
+// Route for get book by id
+app.get('/books/:id', async (request, response) => {
+  try {
+
+    const { id } = request.params; 
+    const book = await Book.findById(id);
+    return response.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message })
+  }
+});
+
 mongoose
   .connect(mongoDBURL)
   .then(() => {
