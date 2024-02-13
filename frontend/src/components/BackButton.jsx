3884854deffc20a0fc14bb7c4c1b30e78645ebb4
Doheera-kosi <kupourevans99@gmail.com>
2024-02-13 19:08:10 +0000

    Added Delete into frontend

--- frontend/src/components/BackButton.jsx
+++ frontend/src/components/BackButton.jsx
@@ -1,18 +1,22 @@
 import { BsArrowLeft } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import PropTypes from 'prop-types'; // Import PropTypes
 
-
-Link
 const BackButton = ({ destination = '/' }) => {
   return (
     <div className='flex'>
       <Link to={destination}
-      className='bg-sky-800 text-white px-4 py-1 rounded-lg w-fit'
+        className='bg-sky-800 text-white px-4 py-1 rounded-lg w-fit'
       >
         <BsArrowLeft className='text-2xl' />
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default BackButton
+// Define prop types for the BackButton component
+BackButton.propTypes = {
+  destination: PropTypes.string // Define destination prop type as string
+};
+
+export default BackButton;
