46c4f4b99e8f4804346b847a1719603f35d523e9
Doheera-kosi <kupourevans99@gmail.com>
2024-02-13 11:33:22 +0000

    Add CORS policy

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,27 @@
 import express, { response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 import { Book } from "./models/bookModel.js";
-import booksRoute from './routes/booksRoute.js'
-
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
 
+// Middleware for parsing request body
 app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow Origins with Default of cors(*)
+app.use(cors());
+
+// Option 2: Allow Custom Origins
+app.use(
+  cors({
+    origin: 'http://localhost:5555',
+    methods:['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  })
+)
 
 app.get('/', (request, response) => {
   console.log(request)
